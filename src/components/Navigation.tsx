c156d02b33777c9749aa3e0b61992ec4eadf1afb
chrixzam <chrixtian.zam@gmail.com>
2025-09-13 22:16:26 -0700

    Fix broken logo path

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -44,7 +44,7 @@ export function Navigation({
             
             {/* Brand */}
             <div className="flex items-center space-x-3">
-              <Logo size={40} src="/logo.png" />
+              <Logo size={40} />
               <div>
                 <h1 className="text-xl font-bold bg-gradient-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent">
                   Consense.us
