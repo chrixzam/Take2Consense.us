a229cf820f217cb65dbe7f76437537aeeb578877
chrixzam <chrixtian.zam@gmail.com>
2025-09-13 22:15:52 -0700

    Update logo to use specific image file

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -10,7 +10,7 @@ export function Logo({ size = 40, className, src }: LogoProps) {
   const pixelSize = `${size}px`;
   return (
     <img
-      src={src || '/logo.png'}
+      src={src || '/cuh_risz_httpss.mj.runfqKC_SdUm2Y_Recreate_this_logo_in_a_cal_fdde79b4-4e5f-47a5-a78c-24b17daddfdd_3.jpg'}
       alt="Consense.us logo"
       width={pixelSize}
       height={pixelSize}
