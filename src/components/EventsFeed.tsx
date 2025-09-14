be296e2db58cdc36633ecb3e38fc7c17d3a1c473
chrixzam <chrixtian.zam@gmail.com>
2025-09-13 21:42:55 -0700

    EventsFeed: center inner container using flex justify-center with w-fit

--- src/components/EventsFeed.tsx
+++ src/components/EventsFeed.tsx
@@ -141,7 +141,8 @@ export function EventsFeed({
       )}
 
       {canFetch && (
-        <div className="bg-white/[0.78] rounded-2xl shadow-sm border border-gray-200 inline-block w-fit mx-auto">
+        <div className="flex justify-center">
+          <div className="bg-white/[0.78] rounded-2xl shadow-sm border border-gray-200 w-fit">
           {loading ? (
             <div className="p-6 text-sm text-gray-600">Loading events…</div>
           ) : error ? (
@@ -216,6 +217,7 @@ export function EventsFeed({
               })}
             </ul>
           )}
+          </div>
         </div>
       )}
       </div>
